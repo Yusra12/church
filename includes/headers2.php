4df012baf70bef805b94729ea915ed50c3f13312
Yusra12 <54258359+Yusra12@users.noreply.github.com>
2019-10-18 17:19:43 +0300

    as

---
+++ includes/headers2.php
@@ -0,0 +1,19 @@
+<nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
+      <div class="container">
+       
+        
+       
+        <div class="collapse navbar-collapse" id="ftco-nav">
+          <ul class="navbar-nav ml-auto">
+            
+            <li class="nav-item active"><a href="appointments.php" class="nav-link">All appointments</a></li>
+            <li class="nav-item active"><a href="newappointment.php" class="nav-link">Accepted appointments</a></li>
+            <li class="nav-item active"><a href="rejected-appointment.php" class="nav-link">Rejected appointments</a></li>
+            
+            <li class="nav-item active"><a href="addservices.php" class="nav-link">Add Services</a></li>
+            
+            
+          </ul>
+        </div>
+      </div>
+    </nav>
